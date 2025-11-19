bc9c211e77354e0f9fad0bf39aa7e776a493e2ce
GitHalex <calex1731@gmail.com>
2025-11-18 20:34:35 -0400

    Added: new fiel with examples of arrays

--- build/js/10-arrays.js
+++ build/js/10-arrays.js
@@ -1,1 +1,9 @@
 "use strict";
+console.log("hola");
+const fruits = ["manzana", "banana", "cereza"];
+fruits.push("naranja");
+console.log(fruits);
+let books = ["El hobbit", 1984];
+let data = ["Hola", 42, true, null];
+let test = [];
+let data2 = ["Texto", 100, false];
---
+++ src/10-arrays.ts
@@ -0,0 +1,13 @@
+console.log("hola");
+const fruits: string[] = ["manzana", "banana", "cereza"];
+fruits.push("naranja");
+console.log(fruits);
+
+let books: (string | number)[] = ["El hobbit", 1984];
+
+let data = ["Hola", 42, true, null];
+
+let test = [];
+
+// tuplas
+let data2: [string, number, boolean] = ["Texto", 100, false];
